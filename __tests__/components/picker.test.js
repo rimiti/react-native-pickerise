8b142df1f7065a7b3416641f69ec868816bf5a8e
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2017-10-27 13:10:17 +0200

    chore(__tests__/components/picker.tests.js) test created

---
+++ __tests__/components/picker.test.js
@@ -0,0 +1,23 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Pickerise from '../../src/components/picker';
+
+describe('Pickerise', () => {
+  it('Renders correctly', () => {
+    const items = [
+      {section: true, label: 'Cars'}, {label: 'Audi'}, {label: 'Dodge'}, {label: 'Ford'}, {label: 'Renault'},
+      {section: true, label: 'Bikes'}, {label: 'Kawasaki'}, {label: 'Suzuki'}, {label: 'Triumph'}];
+
+    const tree = renderer.create(
+      <Pickerise
+        items={items}
+        initValue="Select me"
+        onChange={(option) => {
+        }}
+      />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
